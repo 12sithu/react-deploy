fb8164707dbd40819e5b27e67a15ea31cd3d28a6
sithu <sithuhein200295@gmail.com>
2020-08-05 16:25:57 +0630

    add react app to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sithu</h1>
+      <p>test react app</p>
+    </div>
+  );
+}
+
+export default App;
